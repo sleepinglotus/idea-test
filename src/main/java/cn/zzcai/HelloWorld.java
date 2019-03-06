b2b7fba78d054f3684c4da284b794649ff787644
sleepinglotus <1012758596@qq.com>
2019-03-06 11:00:21 +0800

    test git

---
+++ src/main/java/cn/zzcai/HelloWorld.java
@@ -0,0 +1,12 @@
+package cn.zzcai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorld {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorld.class,args);
+        //System.out.println("a");
+    }
+}
---
+++ src/main/java/cn/zzcai/service/HelloService.java
@@ -0,0 +1,4 @@
+package cn.zzcai.service;
+
+public class HelloService {
+}
